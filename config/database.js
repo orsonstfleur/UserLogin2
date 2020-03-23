ad22aa4b555aa2b52ba2da9920c24b51054f8963
Orson Saint Fleur <orsonsaintfleurgmail.com>
2020-03-23 09:36:14 -0400

    first

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+   'url' : 'mongodb+srv://personal2:personal2@personallogin2-ann9x.mongodb.net/test?retryWrites=true&w=majority',
+   'dbName' : 'personal2'
+};
